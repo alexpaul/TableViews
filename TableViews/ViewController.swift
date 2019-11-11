271242ad62aa4f4e22624c04a2427cd163696120
alexpaul <alexpaulnyc@gmail.com>
2019-11-11 18:14:25 -0500

    fix typo

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  // Topics covered:
   /*
-   - UITableView
-   - cells can be static or dynamic
-   - prototype cell
-   - reuse identifier e.g "countryCell"
-   - dataSource object (WE NEED TO SET THE DATASOURCE OBJECT e.g tableView.dataSource = self), configured in viewDidLoad()
-   - UITableViewDataSource - 2 required methods
-   - first required methdod is numberOfRows()
-   - second required methods is cellForRow()
-   - dequeueCell - recycles a cell if in memory, if not it creates a new cell
+   ## Topics Covered
+   
+   * UITableView
+   * cells can be static or dynamic
+   * prototype cell
+   * reuse identifier e.g "countryCell"
+   * dataSource object (WE NEED TO SET THE DATASOURCE OBJECT e.g tableView.dataSource = self), configured in viewDidLoad()
+   * UITableViewDataSource - 2 required methods
+   * first required method is numberOfRows()
+   * second required methods is cellForRow()
+   * dequeueCell - recycles a cell if in memory, if not it creates a new cell
   */
   
   // MARK:- outlets and properties
